5471a5fa5ab0180221f7897f4d91272564b0d7b2
Sivalojan <sivalojan862001@gmail.com>
2025-07-01 12:57:40 +0530

    Initial commit

--- app/src/main/java/com/example/myapplication02/SecondActivity.java
+++ app/src/main/java/com/example/myapplication02/SecondActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.myapplication02.R;
+
 
 public class SecondActivity extends AppCompatActivity {
     TextView textViewGreeting;
